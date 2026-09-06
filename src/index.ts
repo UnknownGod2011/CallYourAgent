32da173f171db8bf3d52bd6eb152a4367095af48
UnknownGod2011 <tanushshah2006@gmail.com>
2026-09-06 19:35:09 +0530

    feat: export CALL-E webhook parser

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ export * from "./domain.js";
 export * from "./store.js";
 export * from "./call-provider.js";
 export * from "./calle-provider.js";
+export * from "./calle-webhook.js";
 export * from "./control-plane.js";
